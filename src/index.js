608b04992cb81789c47e9d3026e4c01b6d8323ce
sandaruwan98 <lakshan.jayasinghe@pagero.com>
2025-02-20 19:10:48 +0530

    Sticky card effect added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import theme from './theme';
 
